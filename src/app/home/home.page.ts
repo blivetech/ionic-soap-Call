ae2645d46d5a98a07b41cc503772a1d57b55fed2
blivetech <47708490+blivetech@users.noreply.github.com>
2020-11-26 13:27:25 +0530

    v1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,58 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  username:any;
+  password:any;
+
+  constructor() {
+  //  this.soapCall();
+  }
+
+  soapCall() {
+
+    if(this.username != null || this.username != undefined && this.password != null || this.password != undefined){
+
+
+
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', '', true);
+   // const input_element = <HTMLInputElement> document.getElementById('choosenNumber');
+    // The following variable contains the xml SOAP request.
+    const sr = `<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <IndusMobileUserLogin1 xmlns="http://tempuri.org/">
+      <UserName>`+this.username+`</UserName>
+      <UserPassword>`+this.password+`</UserPassword>
+    </IndusMobileUserLogin1>
+  </soap:Body>
+</soap:Envelope>`;
+
+    xmlhttp.onreadystatechange =  () => {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                const xml = xmlhttp.responseXML;
+                // Here I'm getting the value contained by the <return> node.
+            //    const response_number = parseInt(xml.getElementsByTagName('return')[0].childNodes[0].nodeValue);
+                // Print result square number.
+                var s = new XMLSerializer();
+              var newXmlStr = s.serializeToString(xml);      
+              alert(newXmlStr);
+            }else{
+              console.log(xmlhttp.status);
+            }
+
+        }
+    }
+    // Send the POST request.
+    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+    xmlhttp.responseType = 'document';
+    xmlhttp.send(sr);
+  }else{
+    alert("Invalid Username or Password");
+  }
+
+  }
+
 
 }
